b00a4ed88d73639ac9b7bdb10186e8a07c80d523
Fernando <ferweb.reyna@gmail.com>
2023-06-20 22:49:02 -0300

    logger

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/", express.static(`${__dirname}/public`));
 app.use(morgan("dev"));
+app.use(loggerMiddleware);
 
 app.use(session({
     store: MongoStore.create({
